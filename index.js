9ea9eecda60480620a63990290a4a9b28faf993a
JuliusWon <julius.won3@gmail.com>
2022-03-23 13:52:43 -0400

    color

--- index.js
+++
@@ -1,4 +0,0 @@
-var urls = ["https://nourek.itch.io/","https://github.com/JuliusWon/","https://sidequestvr.com/app/1613/laser-arcade-vr","https://www.youtube.com/channel/UCpGiOJXgVeUbdEncJF9lNmw"]
-function OpenRes(number){
-	window.open(urls[number])
-}
